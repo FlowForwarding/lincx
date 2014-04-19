6d85da24e0df5fbdd323f1c98c60efc6ce6de036
root <root@linc2.atg.inca.infoblox.com>
2014-04-19 13:57:57 -0700

    mailbox_limit option added when opening vifs

--- apps/linc_max/src/linc_max_port_native.erl
+++ apps/linc_max/src/linc_max_port_native.erl
@@ -7,10 +7,12 @@
 -module(linc_max_port_native).
 -export([vif/1]).
 
+-define(VIF_MAILBOX_LIMIT, 131072).
+
 -spec vif(string()) -> {ok, port(), binary()} |
 					   {error, term()}.
 vif(Interface) ->
-	case net_vif:open(Interface, [binary]) of
+	case net_vif:open(Interface, [binary,{mailbox_limit,?VIF_MAILBOX_LIMIT}]) of
 	{ok,Port} ->
 		HwAddr = get_hw_addr(Interface),
 		{ok,Port,HwAddr};
