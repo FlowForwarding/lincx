7a0bb67732b5de30499004f2d4f84c96108aeb97
root <root@linc7.atg.inca.infoblox.com>
2014-05-15 12:23:21 -0700

    reconnect_ports() fix using #port_info{}

--- apps/linc_max/src/linc_max_fast_path.erl
+++ apps/linc_max/src/linc_max_fast_path.erl
@@ -219,7 +219,7 @@ reignite(#blaze{ports =Ports} =Blaze) ->
 	drain_packets(NewPid).
 
 reconnect_ports(Ports, NewPid) ->
-	lists:foreach(fun({_,Outlet,_}) ->
+	lists:foreach(fun(#port_info{outlet =Outlet}) ->
 		erlang:port_connect(Outlet, NewPid),
 		unlink(Outlet)
 	end, Ports).
