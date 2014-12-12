e1d5adba91762047e48793ac4431a31e103e0ccc
krant <aleksey.vasilenko@gmail.com>
2014-12-12 16:24:53 +0200

    Beter fix to previous fix

--- apps/linc/src/default_railing.erl
+++ apps/linc/src/default_railing.erl
@@ -130,7 +130,13 @@ yml(Cfg) ->
 			proplists:get_value("9p", Opts, [])
 		),
 
-	Secret = "-secret " ++ proplists:get_value("secret", Opts, ""),
+	Secret =
+		case proplists:get_value("secret", Opts) of
+			undefined ->
+				"";
+			S ->
+				"-secret " ++ S
+		end,
 
 	conf(Ports, Queues, Controllers, Ipconf, ListenIp, ListenPort, Memory, NineP ++ " " ++ Secret).
 
