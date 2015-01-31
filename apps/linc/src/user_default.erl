50f47c3c6df7b050d017b95c15b79b279cbfa92f
Maxim Kharchenko <maxim.kharchenko@gmail.com>
2015-01-31 05:08:59 +0200

    vif() output expanded (fixed)

--- apps/linc/src/user_default.erl
+++ apps/linc/src/user_default.erl
@@ -3,15 +3,14 @@
 
 vif() ->
 	{ok,Ifs} = inet:getifaddrs(),
-	io:format("Name     Mac               Address         Netmask         Gateway\n"),
-			%% 01234567 00:00:00:00:00:00 192.192.192.192 255.255.255.255 100.100.100.100
+	io:format("Name     Mac               Address         Netmask\n"),
+			%% 01234567 00:00:00:00:00:00 192.192.192.192 255.255.255.255
 	lists:foreach(fun({Name,Props}) ->
 						Mac	    = proplists:get_value(hwaddr, Props, [0,0,0,0,0,0]),
 						IpAddr  = proplists:get_value(addr, Props),
 						Netmask = proplists:get_value(netmask, Props),
-						Gateway = proplists:get_value(dstaddr, Props),
-						io:format("~-8s ~s ~-15s ~-15s ~-15s\n",
-									[Name,hex(Mac),ip(IpAddr),ip(Netmask),ip(Gateway)]) end, Ifs).
+						io:format("~-8s ~s ~-15s ~-15s\n",
+									[Name,hex(Mac),ip(IpAddr),ip(Netmask)]) end, Ifs).
 
 hex(Mac) ->
 	string:join([ io_lib:format("~2.16.0b", [X]) || X <- Mac ], ":").
